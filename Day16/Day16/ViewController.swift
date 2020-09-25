d74ddb6addbceca6dfb95068ed213b9f6be645c5
Chris Harding <chrisharding.m@gmail.com>
2020-09-25 09:44:34 -0600

    New push

--- Day16/Day16/ViewController.swift
+++ Day16/Day16/ViewController.swift
@@ -8,10 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        print(pictures)
+        }
         // Do any additional setup after loading the view.
     }
 
